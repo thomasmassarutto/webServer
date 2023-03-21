356de87c7a15df35bcb130c14cf4146ed2c23a89
thomasmassarutto <thomas.massarutto@gmail.com>
2023-03-21 09:21:09 +0100

    Es1

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <body>
+        <h1>es1</h1>
+        <p>
+            <?php
+            $a= prova;
+            $b=18;
+            $c= $b+77;
+            echo "Questa &egrave; una $a</p>";
+            echo "<p>Valore di c:$c</p>"; 
+            ?>
+    </body>
+</head>
+
+</html>
