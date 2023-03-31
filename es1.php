aa4d1003ac04eec76bb3ef2c0d46b5fff0bb0cb9
Ubuntu <ubuntu@ip-172-31-28-226.eu-central-1.compute.internal>
2023-03-31 11:32:10 +0000

    msg

---
+++ es1.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>Esercizio 1</head>
+
+<body>
+    <h1>Esercizio 1</h1>
+    <p>
+        <?php
+        $a = "prova";
+        $b = 18;
+        $c = $b + 77;
+        echo "Questa &egrave; una $a</p>";
+        echo "<p>Valore di c: $c</p>";
+        ?>
+</body>
+
+</html>
